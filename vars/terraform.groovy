50c2136124a730276ddb9ba5db00c74077442140
KalluruS <>
2021-02-09 08:06:56 +0530

    Update jenkins/

---
+++ vars/terraform.groovy
@@ -0,0 +1,30 @@
+def call(action) {
+  if (action == "apply") {
+    pipeline {
+      agent any
+
+      stages {
+
+        stage('INIT') {
+          steps {
+            sh '''
+              export AWS_ACCESS_KEY_ID=${AWS_USR}
+              export AWS_SECRET_ACCESS_KEY=${AWS_PSW}
+              make dev-init
+            '''
+          }
+        }
+        stage('APPLY') {
+          steps {
+            sh '''
+              export AWS_ACCESS_KEY_ID=${AWS_USR}
+              export AWS_SECRET_ACCESS_KEY=${AWS_PSW}
+              make dev-apply
+            '''
+          }
+        }
+
+      }
+    }
+  }
+}
