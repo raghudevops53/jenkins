f5edc0eab602c2ce0979b184a41801c2c674563a
KalluruS <>
2021-02-09 08:31:23 +0530

    Update jenkins/

--- seed/terraform.groovy
+++ seed/terraform.groovy
@@ -45,3 +45,24 @@ pipelineJob('Terraform-Releases/FRONTEND') {
     }
   }
 }
+
+pipelineJob('Terraform-Releases/ALB') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-alb')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
