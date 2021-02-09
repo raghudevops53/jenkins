a605d0372ca5cd318c37d765c286a64a424c76c0
KalluruS <>
2021-02-09 08:29:50 +0530

    Update jenkins/

--- seed/terraform.groovy
+++ seed/terraform.groovy
@@ -24,3 +24,24 @@ pipelineJob('Terraform-Releases/VPC') {
     }
   }
 }
+
+pipelineJob('Terraform-Releases/FRONTEND') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-frontend')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
