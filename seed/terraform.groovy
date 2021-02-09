1ada6c46cf35cb2224e0a517a83a3a803c96e63e
KalluruS <>
2021-02-09 08:28:30 +0530

    Update jenkins/

--- seed/terraform.groovy
+++ seed/terraform.groovy
@@ -1,8 +1,26 @@
-pipelineJob('example') {
-  definition {
-    cps {
-      script(readFileFromWorkspace('project-a-workflow.groovy'))
-      sandbox()
+///
+folder('Terraform-Releases') {
+  displayName('Terraform-Releases')
+  description('Terraform-Releases')
+}
+
+pipelineJob('Terraform-Releases/VPC') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-vpc')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
     }
   }
 }
